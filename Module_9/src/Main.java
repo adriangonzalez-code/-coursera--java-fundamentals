2f1715081d29eb6a723aed6f4bb0c59d2c2d2a8a
Adrián González <g.adrian.gonzalez.90@gmail.com>
2025-03-12 22:02:25 -0600

    Operators Precedence

---
+++ Module_9/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num1 = 50, num2 = 100, num3 = 30;
+
+        int result = num1 + num2 * num3 / 10;
+
+        System.out.println("Result: " + result);
+    }
+}
